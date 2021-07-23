ab58deded8e1e54b986328b74132a07ff70368b7
Manuj Grover <manujgrover71@gmail.com>
2021-07-23 11:21:21 +0530

    Completed Problem1 (Swap numbers without using third variable in C)

---
+++ C Foundations/swap_nums.c
@@ -0,0 +1,33 @@
+/*
+ * Program to swap two numbers without using third variable.
+ * Compilation: gcc -o swap_nums swap_nums.c
+ * Exection: ./swap_nums
+ *
+ * @autor: Manuj, @Roll_Number: 1910990170
+ * Date: 23/07/2021
+*/
+
+#include <stdio.h>
+
+/*
+ * Function that take two variables by reference and swap them.
+*/ 
+void swap(int *a, int *b) {
+    (*a) += (*b);
+    (*b) = (*a) - (*b);
+    (*a) -= (*b);
+}
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    // print the numbers before swapping.
+    printf("Before swap\n a=%d, b=%d\n", a, b);
+
+    // pass the number by reference.
+    swap(&a, &b);
+
+    // print the numbers after swapping.
+    printf("After swap\n a=%d, b=%d\n", a, b);
+}
